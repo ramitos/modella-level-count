a1b4c38203dbbb8f89dbef04065ac3f2e8710d6d
Sérgio Ramos <mail@sergioramos.me>
2013-12-28 15:26:07 -0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,22 @@
+# modella-level-count
+
+[![NPM version](https://badge.fury.io/js/modella-level-count.png)](http://badge.fury.io/js/modella-level-count)
+[![Dependency Status](https://gemnasium.com/ramitos/modella-level-count.png)](https://gemnasium.com/ramitos/modella-level-count)
+
+## install
+
+```bash
+npm install [--save/--save-dev] modella-level-count
+```
+
+## api
+
+```js
+Model.use(db, function(model){
+  return model.should_be_counted()
+})
+
+Model.count() // => number of models
+``
+
+## license
---
+++ src/modella-level-count.js
@@ -0,0 +1,45 @@
+var interpolate = require('util').format
+var type = require('type-component')
+
+var options = {
+  keyEncoding: 'utf8',
+  valueEncoding: 'utf8'
+}
+
+module.exports = function(db, toCount){
+  if (type(toCount) !== 'function') toCount = function(){return true}
+
+  return function(Model){
+    var key = interpolate('/count/' + Model.modelName)
+    var count = 0
+
+    db.get(key, function(err, value){
+      if (err && err.type !== 'NotFoundError') throw err
+      count += Number(value) || 0
+    })
+
+    Model.on('saving', function(instance, fn){
+      if (!toCount(instance)) return fn()
+
+      instance.model.db.get(instance.primary(), function(err){
+        if (err && err.type !== 'NotFoundError') return fn(err)
+        count += err ? 1 : 0
+        db.put(key, count, options, fn)
+      })
+    })
+
+    Model.on('removing', function(instance, fn){
+      if (!toCount(instance)) return fn()
+
+      instance.model.db.get(instance.primary(), function(err){
+        if (err && err.type !== 'NotFoundError') return fn(err)
+        count -= err ? 0 : 1
+        db.put(key, count, options, fn)
+      })
+    })
+
+    Model.count = function(){
+      return count
+    }
+  }
+}
